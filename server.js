9cff95e9b7cb3458a41c48e765ffb60a79f89043
Abhishek Barkade <abhibarkade111@gmail.com>
2021-11-04 01:57:48 +0530

    updated

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const app =express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const path = require('path')
 const mongoose = require('mongoose')
 
